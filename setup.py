313f84de78013032565e5716ceaeb2e8321e874d
[Ankur Kasare] <ankur.kasare.23@gmail.com>
2026-06-21 18:43:06 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+Hypen_e_dot = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Ankur',
+    author_email='ankur.kasare.23@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
